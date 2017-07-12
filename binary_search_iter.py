d900883b1d166a8dd22203e868db6167f236e6f9
lironhp <lironhp@gmail.com>
2017-07-12 19:00:06 +0300

    Update binary_search_iter.py

--- binary_search_iter.py
+++ binary_search_iter.py
@@ -1,6 +1,32 @@
+import unittest
+
+
+
 #GLOBALS
 #=======
 FIRST_IDX = 0
+
+class TestBinarySearch(unittest.TestCase):
+	def test_chop(self):
+		self.assertEqual(-1, chop(3, []))
+		self.assertEqual(-1, chop(3, [1]))
+		self.assertEqual(0,  chop(1, [1, 3, 5]))
+		self.assertEqual(1,  chop(3, [1, 3, 5]))
+		self.assertEqual(2,  chop(5, [1, 3, 5]))
+		self.assertEqual(-1, chop(0, [1, 3, 5]))
+		self.assertEqual(-1, chop(2, [1, 3, 5]))
+		self.assertEqual(-1, chop(4, [1, 3, 5]))
+		self.assertEqual(-1, chop(6, [1, 3, 5]))
+		
+		self.assertEqual(0,  chop(1, [1, 3, 5, 7]))
+		self.assertEqual(1,  chop(3, [1, 3, 5, 7]))
+		self.assertEqual(2,  chop(5, [1, 3, 5, 7]))
+		self.assertEqual(3,  chop(7, [1, 3, 5, 7]))
+		self.assertEqual(-1, chop(0, [1, 3, 5, 7]))
+		self.assertEqual(-1, chop(2, [1, 3, 5, 7]))
+		self.assertEqual(-1, chop(4, [1, 3, 5, 7]))
+		self.assertEqual(-1, chop(6, [1, 3, 5, 7]))
+		self.assertEqual(-1, chop(8, [1, 3, 5, 7]))
 
 def chop(number, int_list):
 	list_size = len(int_list)
@@ -22,26 +48,8 @@ def chop(number, int_list):
 		
 	return -1
 			
-def test_chop():
-	print(-1, chop(3, []))
-	print(-1, chop(3, [1]))
-	print(0,  chop(1, [1, 3, 5]))
-	print(1,  chop(3, [1, 3, 5]))
-	print(2,  chop(5, [1, 3, 5]))
-	print(-1, chop(0, [1, 3, 5]))
-	print(-1, chop(2, [1, 3, 5]))
-	print(-1, chop(4, [1, 3, 5]))
-	print(-1, chop(6, [1, 3, 5]))
-	print(0,  chop(1, [1, 3, 5, 7]))
-	print(1,  chop(3, [1, 3, 5, 7]))
-	print(2,  chop(5, [1, 3, 5, 7]))
-	print(3,  chop(7, [1, 3, 5, 7]))
-	print(-1, chop(0, [1, 3, 5, 7]))
-	print(-1, chop(2, [1, 3, 5, 7]))
-	print(-1, chop(4, [1, 3, 5, 7]))
-	print(-1, chop(6, [1, 3, 5, 7]))
-	print(-1, chop(8, [1, 3, 5, 7]))
+
   
   
 if __name__ == "__main__":
-	test_chop()
+	unittest.main()
